ab1a5f4708961d372bf62e56b8ce8fe1f14d102b
CatalinCJS <cjstan999@gmail.com>
2024-07-27 19:33:42 +0300

    js first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+/////TEMA1
+
+/*function divisibleByTen(num){
+  return num % 10 === 0 ? true : false;
+  
+}
+
+console.log(divisibleByTen(20));
+console.log(divisibleByTen(100));
+console.log(divisibleByTen(24));
+console.log(divisibleByTen(258));*/
+
+///TEMA2
+//-ex1
+ /* const tellFortune = 'Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.';
+
+  console.log(`${tellFortune}`);*/
+//-ex2
+/*const tellFortune = "Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.";
+console.log(tellFortune);*/
+
+//-ex3
+/*function tellFortune(N, Z, Y, X) {
+console.log(`Vei fi un ${Z} de ${X}, căsătorit cu ${Y} și vei avea ${N} premii.`)
+  }
+  tellFortune(4, "Pictor", "Emma" , "succes");*/
+
+///TEMA3
+/*function weekDays(a){
+  switch(a){
+  case 1:
+      return "Luni";
+  case 2:
+      return "Marti";
+  case 3:
+      return "Miercuri";
+  case 4:
+      return "Joi";
+  case 5:
+      return "Vineri";
+  case 6:
+      return "Sambata";
+  case 7:
+      return "Duminica";
+  default:
+     return "Saptamana are doar 7 zile";
+  }
+}
+
+    console.log(weekDays(4));
+    console.log(weekDays(6));
+    console.log(weekDays(1));
+    console.log(weekDays(10));*/
+
+
+
