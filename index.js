7b011a125814460b4a291d63ef2af712b0f9f5af
CatalinCJS <cjstan999@gmail.com>
2024-07-28 17:16:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /////TEMA1
 
-/*function divisibleByTen(num){
+function divisibleByTen(num){
   return num % 10 === 0 ? true : false;
   
 }
@@ -8,25 +8,25 @@
 console.log(divisibleByTen(20));
 console.log(divisibleByTen(100));
 console.log(divisibleByTen(24));
-console.log(divisibleByTen(258));*/
+console.log(divisibleByTen(258));
 
 ///TEMA2
 //-ex1
- /* const tellFortune = 'Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.';
+  const tellFortune = 'Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.';
 
-  console.log(`${tellFortune}`);*/
+  console.log(`${tellFortune}`);
 //-ex2
-/*const tellFortune = "Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.";
-console.log(tellFortune);*/
+const tellFortune = "Vei fi un Programator în Italia, căsătorit cu Emanuel și vei avea 3 copii.";
+console.log(tellFortune);
 
 //-ex3
-/*function tellFortune(N, Z, Y, X) {
+function tellFortune(N, Z, Y, X) {
 console.log(`Vei fi un ${Z} de ${X}, căsătorit cu ${Y} și vei avea ${N} premii.`)
   }
-  tellFortune(4, "Pictor", "Emma" , "succes");*/
+  tellFortune(4, "Pictor", "Emma" , "succes");
 
 ///TEMA3
-/*function weekDays(a){
+function weekDays(a){
   switch(a){
   case 1:
       return "Luni";
@@ -50,7 +50,7 @@ console.log(`Vei fi un ${Z} de ${X}, căsătorit cu ${Y} și vei avea ${N} premi
     console.log(weekDays(4));
     console.log(weekDays(6));
     console.log(weekDays(1));
-    console.log(weekDays(10));*/
+    console.log(weekDays(10));
 
 
 
